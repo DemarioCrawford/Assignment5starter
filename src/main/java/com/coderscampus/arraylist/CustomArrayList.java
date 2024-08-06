912dc6ad8b18c2d0debd5963346f6ef6241eca07
DemarioCrawford <Rooneymusic4@gmail.com>
2024-08-05 21:31:20 -0700

    Update CustomArrayList.java

--- src/main/java/com/coderscampus/arraylist/CustomArrayList.java
+++ src/main/java/com/coderscampus/arraylist/CustomArrayList.java
@@ -2,22 +2,23 @@ package com.coderscampus.arraylist;
 
 public class CustomArrayList<T> implements CustomList<T> {
 	Object[] items = new Object[10];
-
+	private int size = 0;
+	
 	@Override
 	public boolean add(T item) {
-		// TODO Auto-generated method stub
+		
 		return false;
 	}
 
 	@Override
 	public int getSize() {
-		// TODO Auto-generated method stub
+		
 		return 0;
 	}
 
 	@Override
 	public T get(int index) {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 	
